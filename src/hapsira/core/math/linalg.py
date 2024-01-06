07db36939c69965e0adf66dfd313c3e82755f380
Sebastian M. Ernst <ernst@pleiszenburg.de>
2024-01-06 14:43:46 +0100

    jit iod

--- src/hapsira/core/math/linalg.py
+++ src/hapsira/core/math/linalg.py
@@ -17,6 +17,11 @@ __all__ = [
 
 
 @hjit("V(V,V)")
+def add_VV_hf(a, b):
+    return a[0] + b[0], a[1] + b[1], a[2] + b[2]
+
+
+@hjit("V(V,V)")
 def cross_VV_hf(a, b):
     return (
         a[1] * b[2] - a[2] * b[1],

08cf5f022c614c6f10ac8aa046bd8ff4ca7f16e3
Sebastian M. Ernst <ernst@pleiszenburg.de>
2024-01-13 09:44:20 +0100

    rm class

--- src/hapsira/core/math/ivp/_rk.py
+++ src/hapsira/core/math/ivp/_rk.py
@@ -466,17 +466,23 @@ class OdeSolver:
         raise NotImplementedError
 
 
-class RungeKutta(OdeSolver):
+class DOP853(OdeSolver):
     """Base class for explicit Runge-Kutta methods."""
 
-    C: np.ndarray = NotImplemented
-    A: np.ndarray = NotImplemented
-    B: np.ndarray = NotImplemented
+    n_stages: int = dop853_coefficients.N_STAGES
+    order: int = 8
+    error_estimator_order: int = 7
+    A = dop853_coefficients.A[:n_stages, :n_stages]
+    B = dop853_coefficients.B
+    C = dop853_coefficients.C[:n_stages]
     E: np.ndarray = NotImplemented
+    E3 = dop853_coefficients.E3
+    E5 = dop853_coefficients.E5
+    D = dop853_coefficients.D
     P: np.ndarray = NotImplemented
-    order: int = NotImplemented
-    error_estimator_order: int = NotImplemented
-    n_stages: int = NotImplemented
+
+    A_EXTRA = dop853_coefficients.A[n_stages + 1 :]
+    C_EXTRA = dop853_coefficients.C[n_stages + 1 :]
 
     def __init__(
         self,
@@ -514,11 +520,20 @@ class RungeKutta(OdeSolver):
         self.error_exponent = -1 / (self.error_estimator_order + 1)
         self.h_previous = None
 
-    def _estimate_error(self, K, h):
-        return np.dot(K.T, self.E) * h
+        self.K_extended = np.empty(
+            (dop853_coefficients.N_STAGES_EXTENDED, self.n), dtype=self.y.dtype
+        )
+        self.K = self.K_extended[: self.n_stages + 1]
 
     def _estimate_error_norm(self, K, h, scale):
-        return norm(self._estimate_error(K, h) / scale)
+        err5 = np.dot(K.T, self.E5) / scale
+        err3 = np.dot(K.T, self.E3) / scale
+        err5_norm_2 = np.linalg.norm(err5) ** 2
+        err3_norm_2 = np.linalg.norm(err3) ** 2
+        if err5_norm_2 == 0 and err3_norm_2 == 0:
+            return 0.0
+        denom = err5_norm_2 + 0.01 * err3_norm_2
+        return np.abs(h) * err5_norm_2 / np.sqrt(denom * len(scale))
 
     def _step_impl(self):
         t = self.t
@@ -586,152 +601,6 @@ class RungeKutta(OdeSolver):
 
         return True, None
 
-
-class DOP853(RungeKutta):
-    """Explicit Runge-Kutta method of order 8.
-
-    This is a Python implementation of "DOP853" algorithm originally written
-    in Fortran [1]_, [2]_. Note that this is not a literate translation, but
-    the algorithmic core and coefficients are the same.
-
-    Can be applied in the complex domain.
-
-    Parameters
-    ----------
-    fun : callable
-        Right-hand side of the system. The calling signature is ``fun(t, y)``.
-        Here, ``t`` is a scalar, and there are two options for the ndarray ``y``:
-        It can either have shape (n,); then ``fun`` must return array_like with
-        shape (n,). Alternatively it can have shape (n, k); then ``fun``
-        must return an array_like with shape (n, k), i.e. each column
-        corresponds to a single column in ``y``. The choice between the two
-        options is determined by `vectorized` argument (see below).
-    t0 : float
-        Initial time.
-    y0 : array_like, shape (n,)
-        Initial state.
-    t_bound : float
-        Boundary time - the integration won't continue beyond it. It also
-        determines the direction of the integration.
-    first_step : float or None, optional
-        Initial step size. Default is ``None`` which means that the algorithm
-        should choose.
-    max_step : float, optional
-        Maximum allowed step size. Default is np.inf, i.e. the step size is not
-        bounded and determined solely by the solver.
-    rtol, atol : float and array_like, optional
-        Relative and absolute tolerances. The solver keeps the local error
-        estimates less than ``atol + rtol * abs(y)``. Here `rtol` controls a
-        relative accuracy (number of correct digits), while `atol` controls
-        absolute accuracy (number of correct decimal places). To achieve the
-        desired `rtol`, set `atol` to be smaller than the smallest value that
-        can be expected from ``rtol * abs(y)`` so that `rtol` dominates the
-        allowable error. If `atol` is larger than ``rtol * abs(y)`` the
-        number of correct digits is not guaranteed. Conversely, to achieve the
-        desired `atol` set `rtol` such that ``rtol * abs(y)`` is always smaller
-        than `atol`. If components of y have different scales, it might be
-        beneficial to set different `atol` values for different components by
-        passing array_like with shape (n,) for `atol`. Default values are
-        1e-3 for `rtol` and 1e-6 for `atol`.
-    vectorized : bool, optional
-        Whether `fun` is implemented in a vectorized fashion. Default is False.
-
-    Attributes
-    ----------
-    n : int
-        Number of equations.
-    status : string
-        Current status of the solver: 'running', 'finished' or 'failed'.
-    t_bound : float
-        Boundary time.
-    direction : float
-        Integration direction: +1 or -1.
-    t : float
-        Current time.
-    y : ndarray
-        Current state.
-    t_old : float
-        Previous time. None if no steps were made yet.
-    step_size : float
-        Size of the last successful step. None if no steps were made yet.
-    nfev : int
-        Number evaluations of the system's right-hand side.
-    njev : int
-        Number of evaluations of the Jacobian. Is always 0 for this solver
-        as it does not use the Jacobian.
-    nlu : int
-        Number of LU decompositions. Is always 0 for this solver.
-
-    References
-    ----------
-    .. [1] E. Hairer, S. P. Norsett G. Wanner, "Solving Ordinary Differential
-           Equations I: Nonstiff Problems", Sec. II.
-    .. [2] `Page with original Fortran code of DOP853
-            <http://www.unige.ch/~hairer/software.html>`_.
-    """
-
-    n_stages = dop853_coefficients.N_STAGES
-    order = 8
-    error_estimator_order = 7
-    A = dop853_coefficients.A[:n_stages, :n_stages]
-    B = dop853_coefficients.B
-    C = dop853_coefficients.C[:n_stages]
-    E3 = dop853_coefficients.E3
-    E5 = dop853_coefficients.E5
-    D = dop853_coefficients.D
-
-    A_EXTRA = dop853_coefficients.A[n_stages + 1 :]
-    C_EXTRA = dop853_coefficients.C[n_stages + 1 :]
-
-    def __init__(
-        self,
-        fun,
-        t0,
-        y0,
-        t_bound,
-        max_step=np.inf,
-        rtol=1e-3,
-        atol=1e-6,
-        vectorized=False,
-        first_step=None,
-        **extraneous,
-    ):
-        super().__init__(
-            fun,
-            t0,
-            y0,
-            t_bound,
-            max_step,
-            rtol,
-            atol,
-            vectorized,
-            first_step,
-            **extraneous,
-        )
-        self.K_extended = np.empty(
-            (dop853_coefficients.N_STAGES_EXTENDED, self.n), dtype=self.y.dtype
-        )
-        self.K = self.K_extended[: self.n_stages + 1]
-
-    def _estimate_error(self, K, h):  # Left for testing purposes.
-        err5 = np.dot(K.T, self.E5)
-        err3 = np.dot(K.T, self.E3)
-        denom = np.hypot(np.abs(err5), 0.1 * np.abs(err3))
-        correction_factor = np.ones_like(err5)
-        mask = denom > 0
-        correction_factor[mask] = np.abs(err5[mask]) / denom[mask]
-        return h * err5 * correction_factor
-
-    def _estimate_error_norm(self, K, h, scale):
-        err5 = np.dot(K.T, self.E5) / scale
-        err3 = np.dot(K.T, self.E3) / scale
-        err5_norm_2 = np.linalg.norm(err5) ** 2
-        err3_norm_2 = np.linalg.norm(err3) ** 2
-        if err5_norm_2 == 0 and err3_norm_2 == 0:
-            return 0.0
-        denom = err5_norm_2 + 0.01 * err3_norm_2
-        return np.abs(h) * err5_norm_2 / np.sqrt(denom * len(scale))
-
     def _dense_output_impl(self):
         K = self.K_extended
         h = self.h_previous
@@ -754,3 +623,86 @@ class DOP853(RungeKutta):
         F[3:] = h * np.dot(self.D, K)
 
         return Dop853DenseOutput(self.t_old, self.t, self.y_old, F)
+
+
+_ = """Explicit Runge-Kutta method of order 8.
+
+This is a Python implementation of "DOP853" algorithm originally written
+in Fortran [1]_, [2]_. Note that this is not a literate translation, but
+the algorithmic core and coefficients are the same.
+
+Can be applied in the complex domain.
+
+Parameters
+----------
+fun : callable
+    Right-hand side of the system. The calling signature is ``fun(t, y)``.
+    Here, ``t`` is a scalar, and there are two options for the ndarray ``y``:
+    It can either have shape (n,); then ``fun`` must return array_like with
+    shape (n,). Alternatively it can have shape (n, k); then ``fun``
+    must return an array_like with shape (n, k), i.e. each column
+    corresponds to a single column in ``y``. The choice between the two
+    options is determined by `vectorized` argument (see below).
+t0 : float
+    Initial time.
+y0 : array_like, shape (n,)
+    Initial state.
+t_bound : float
+    Boundary time - the integration won't continue beyond it. It also
+    determines the direction of the integration.
+first_step : float or None, optional
+    Initial step size. Default is ``None`` which means that the algorithm
+    should choose.
+max_step : float, optional
+    Maximum allowed step size. Default is np.inf, i.e. the step size is not
+    bounded and determined solely by the solver.
+rtol, atol : float and array_like, optional
+    Relative and absolute tolerances. The solver keeps the local error
+    estimates less than ``atol + rtol * abs(y)``. Here `rtol` controls a
+    relative accuracy (number of correct digits), while `atol` controls
+    absolute accuracy (number of correct decimal places). To achieve the
+    desired `rtol`, set `atol` to be smaller than the smallest value that
+    can be expected from ``rtol * abs(y)`` so that `rtol` dominates the
+    allowable error. If `atol` is larger than ``rtol * abs(y)`` the
+    number of correct digits is not guaranteed. Conversely, to achieve the
+    desired `atol` set `rtol` such that ``rtol * abs(y)`` is always smaller
+    than `atol`. If components of y have different scales, it might be
+    beneficial to set different `atol` values for different components by
+    passing array_like with shape (n,) for `atol`. Default values are
+    1e-3 for `rtol` and 1e-6 for `atol`.
+vectorized : bool, optional
+    Whether `fun` is implemented in a vectorized fashion. Default is False.
+
+Attributes
+----------
+n : int
+    Number of equations.
+status : string
+    Current status of the solver: 'running', 'finished' or 'failed'.
+t_bound : float
+    Boundary time.
+direction : float
+    Integration direction: +1 or -1.
+t : float
+    Current time.
+y : ndarray
+    Current state.
+t_old : float
+    Previous time. None if no steps were made yet.
+step_size : float
+    Size of the last successful step. None if no steps were made yet.
+nfev : int
+    Number evaluations of the system's right-hand side.
+njev : int
+    Number of evaluations of the Jacobian. Is always 0 for this solver
+    as it does not use the Jacobian.
+nlu : int
+    Number of LU decompositions. Is always 0 for this solver.
+
+References
+----------
+.. [1] E. Hairer, S. P. Norsett G. Wanner, "Solving Ordinary Differential
+        Equations I: Nonstiff Problems", Sec. II.
+.. [2] `Page with original Fortran code of DOP853
+        <http://www.unige.ch/~hairer/software.html>`_.
+"""

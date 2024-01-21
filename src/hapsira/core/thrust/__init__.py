efb3414934dbc5a72346981e4c5c87740cd511a5
Sebastian M. Ernst <ernst@pleiszenburg.de>
2024-01-21 20:05:53 +0100

    rm exports

--- src/hapsira/core/thrust/__init__.py
+++
@@ -1,9 +0,0 @@
-# from .change_a_inc import change_a_inc_hb
-from .change_argp import change_argp
-from .change_ecc_inc import change_ecc_inc
-
-__all__ = [
-    # "change_a_inc_hb",
-    "change_argp",
-    "change_ecc_inc",
-]

f2a315796d664e103bc86ed891f13be4e8687372
mtryan83 <mtryan83@gmail.com>
2024-11-08 14:33:38 -0800

    Update matplotlib.py - forgot self in commit 41e9fd3

--- src/hapsira/plotting/orbit/backends/matplotlib.py
+++ src/hapsira/plotting/orbit/backends/matplotlib.py
@@ -71,7 +71,7 @@ class Matplotlib2D(OrbitPlotterBackend):
         if color is None:
             try:
                 # Updated HACK, see https://stackoverflow.com/a/78938755
-                color = ax._get_lines.get_next_color()
+                color = self.ax._get_lines.get_next_color()
             except AttributeError:
                 # HACK: https://stackoverflow.com/a/13831816/554319
                 color = next(self.ax._get_lines.prop_cycler)["color"]
